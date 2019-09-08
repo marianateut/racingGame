9c2666e1d06b41663867f3ded024a798658037a8
Mariana Teut <mary_marianailea@yahoo.com>
2019-09-08 14:00:21 +0300

    studied classes,variables,primitive data types and aggregation.

--- src/main/java/org/fastrackit/App.java
+++ src/main/java/org/fastrackit/App.java
@@ -8,6 +8,35 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //this is one-live comment
+        Car car = new Car();
+        car.name = "Dacia ";
+        car.fuelLevel = 60;
+        car.mileage = 8.5;
+        car.doorCount = 5;
+        car.maxSpeed = 180.00;
+        car.fuelType = "Gasoline";
+
+        Engine engine = new Engine();
+        engine.manufacturer = "Renault";
+        engine.capacity = 1600;
+        car.engine = engine;
+
+        System.out.println(car.name);
+        System.out.println(car.traveleDistance);
+        System.out.println(car.running);
+        System.out.println(car.engine);
+
+        Car car2 =new Car();
+        car2.name = "Golf";
+        car.name = "Skoda";
+//        car.name = null;
+        car = car2;
+        //concatenation
+        System.out.println("Car2 name:" + car2.name);
+        System.out.println("Car1 name:" + car.name);
+
+
+
     }
 }
---
+++ src/main/java/org/fastrackit/Car.java
@@ -0,0 +1,16 @@
+package org.fastrackit;
+
+public class Car {
+    String name;
+    Double mileage;
+    Double maxSpeed;
+    String fuelType;
+    double fuelLevel;
+    double traveleDistance;
+    int doorCount;
+    boolean running;
+    Engine engine;
+
+
+
+}
