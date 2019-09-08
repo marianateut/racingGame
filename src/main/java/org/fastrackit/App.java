e068f0cb6ed843d0cd09d39e9240d45e5def4856
Mariana Teut <mary_marianailea@yahoo.com>
2019-09-08 21:47:49 +0300

    studiced methods//added accelerate behaviour for vehicles.

--- src/main/java/org/fastrackit/App.java
+++ src/main/java/org/fastrackit/App.java
@@ -27,6 +27,8 @@ public class App
         System.out.println(car.running);
         System.out.println(car.engine);
 
+        car.accelerate( 60,1);
+
         Car car2 =new Car();
         car2.name = "Golf";
         car.name = "Skoda";
--- src/main/java/org/fastrackit/Vehicle.java
+++ src/main/java/org/fastrackit/Vehicle.java
@@ -8,4 +8,18 @@ public class Vehicle {
     double fuelLevel;
     double traveleDistance;
     boolean running;
+
+    public double accelerate(double speed, double durationInHours) {
+        System.out.println(name + "is accelerating with" + speed + "km/h for" + durationInHours + "h.");
+        double distance = speed * durationInHours;
+        System.out.println("Distance:" + distance);
+        traveleDistance = traveleDistance + distance;
+        //same result as the above stament
+//        traveleDistance += distance;
+        double usedFuel = distance * mileage / 100;
+        System.out.println("used fuel:" + usedFuel);
+        usedFuel -= usedFuel;
+        System.out.println("Remaining fuel:" + fuelLevel);
+        return distance;
+    }
 }
