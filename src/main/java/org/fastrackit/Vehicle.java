69b4b66a1c3618b61c4b043b2644bf37ffec330a
Mariana Teut <mary_marianailea@yahoo.com>
2019-09-16 19:25:50 +0300

    studied if -else statements

--- src/main/java/org/fastrackit/Vehicle.java
+++ src/main/java/org/fastrackit/Vehicle.java
@@ -20,12 +20,28 @@ public class Vehicle {
     }
 
     public double accelerate(double speed, double durationInHours) {
+
+        if (speed >= maxSpeed) {
+            System.out.println("Maximum speed exceeded .");
+            return 0;
+
+
+        }
+        if (fuelLevel <= 0) {
+            System.out.println("You are out of fuel...");
+            return 0;
+
+        }
+
+
+
         System.out.println(name + "is accelerating with" + speed + "km/h for" + durationInHours + "h.");
         double distance = speed * durationInHours;
         System.out.println("Distance:" + distance);
         traveleDistance = traveleDistance + distance;
         //same result as the above stament
 //        traveledDistance += distance;
+
         double usedFuel = distance * mileage / 100;
         System.out.println("used fuel:" + usedFuel);
         usedFuel -= usedFuel;
