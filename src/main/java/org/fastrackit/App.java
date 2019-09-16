26aa5963e5f73b72f62d9b2b8d04a475bbde33cb
Mariana Teut <mary_marianailea@yahoo.com>
2019-09-16 20:45:20 +0300

    studied arraays and classic for loop

--- src/main/java/org/fastrackit/App.java
+++ src/main/java/org/fastrackit/App.java
@@ -5,64 +5,67 @@ package org.fastrackit;
  */
 public class App {
     public static void main(String[] args) {
-        Engine engine = new Engine();
-        engine.manufacturer = "Renault";
-        engine.capacity = 1600;
-        //this is one-live comment
-        Car car = new Car(engine);
-        car.setName ("Dacia");
-        car.setFuelLevel(60);
-        car.setMileage(8.5);
-        car.doorCount = 5;
-        car.setMaxSpeed(180.1);
-        car.setFuelType( "Gasoline");
 
-        System.out.println("Car object:" + car.toString());
-
-
-        System.out.println(car.getName());
-        System.out.println(car.getTraveleDistance());
-        System.out.println(car.isRunning());
-        System.out.println(car.engine);
-
-        car.accelerate(60, 1);
-
-        Engine engine2 = new Engine();
-        engine2.manufacturer = "VW";
-        engine2.capacity = 1600;
-        Car car2 = new Car(engine2);
-        car2.setName("Golf");
-        car.setName("Skoda");
-//        car.name = null;
-        car = car2;
-        //concatenation
-        System.out.println("Car2 name:" + car2.getName());
-        System.out.println("Car1 name:" + car.getName());
-
-
-//        System.out.println("Static variables example");
-//        // we should only call static variables from the clas itself (Car),
-//        //not from a variable containing an object of that class
-//        car.totalCount = 1;
-//        System.out.println("Total count in car :" + car.totalCount);
-//        car2.totalCount = 12;
-//        System.out.println("total count in car after setting car2:" + car.totalCount);
-//        System.out.println("total count in car2:" + car2.totalCount);
-//        System.out.println("total count in car2 class:" + car.totalCount);
-
-        Vehicle hybridCar = new HybridCar();
-        hybridCar.setName("Cheater");
-
-        //method implementation from object's class not from variable's class
-        hybridCar.accelerate( 60,1 );
-        System.out.println("Fuel level:" + hybridCar.getFuelLevel());
-        System.out.println("Traveled distance:" + hybridCar.getTraveleDistance());
-
-        //variable type decids what methods can be invoked
-        //type casting
-        ((HybridCar) hybridCar).fly();
-
-
+        Game game = new Game();
+        game.start();
+//        Engine engine = new Engine();
+//        engine.manufacturer = "Renault";
+//        engine.capacity = 1600;
+//        //this is one-live comment
+//        Car car = new Car(engine);
+//        car.setName ("Dacia");
+//        car.setFuelLevel(60);
+//        car.setMileage(8.5);
+//        car.doorCount = 5;
+//        car.setMaxSpeed(180.1);
+//        car.setFuelType( "Gasoline");
+//
+//        System.out.println("Car object:" + car.toString());
+//
+//
+//        System.out.println(car.getName());
+//        System.out.println(car.getTraveleDistance());
+//        System.out.println(car.isRunning());
+//        System.out.println(car.engine);
+//
+//        car.accelerate(60, 1);
+//
+//        Engine engine2 = new Engine();
+//        engine2.manufacturer = "VW";
+//        engine2.capacity = 1600;
+//        Car car2 = new Car(engine2);
+//        car2.setName("Golf");
+//        car.setName("Skoda");
+////        car.name = null;
+//        car = car2;
+//        //concatenation
+//        System.out.println("Car2 name:" + car2.getName());
+//        System.out.println("Car1 name:" + car.getName());
+//
+//
+////        System.out.println("Static variables example");
+////        // we should only call static variables from the clas itself (Car),
+////        //not from a variable containing an object of that class
+////        car.totalCount = 1;
+////        System.out.println("Total count in car :" + car.totalCount);
+////        car2.totalCount = 12;
+////        System.out.println("total count in car after setting car2:" + car.totalCount);
+////        System.out.println("total count in car2:" + car2.totalCount);
+////        System.out.println("total count in car2 class:" + car.totalCount);
+//
+//        Vehicle hybridCar = new HybridCar();
+//        hybridCar.setName("Cheater");
+//
+//        //method implementation from object's class not from variable's class
+//        hybridCar.accelerate( 60,1 );
+//        System.out.println("Fuel level:" + hybridCar.getFuelLevel());
+//        System.out.println("Traveled distance:" + hybridCar.getTraveleDistance());
+//
+//        //variable type decids what methods can be invoked
+//        //type casting
+//        ((HybridCar) hybridCar).fly();
+//
+//
 
     }
 }
--- src/main/java/org/fastrackit/Game.java
+++ src/main/java/org/fastrackit/Game.java
@@ -2,8 +2,38 @@ package org.fastrackit;
 
 public class Game {
 
-    private  Vehicle competitor1;
+    private Vehicle competitor1;
     private Vehicle competitor2;
+    private Track[] tracks = new Track[3];
 
+    public void start() {
+        initializeTracks();
+        displayTracks();
+    }
 
+    private void initializeTracks() {
+        Track track1 = new Track();
+        track1.setName("Monte Carlo");
+        track1.setLength(300);
+
+        tracks[0] = track1;
+        Track track2 = new Track();
+
+        track2.setName(" Monza ");
+        track2.setLength(400);
+
+        tracks[1] = track2;
+
+    }
+
+    private void displayTracks() {
+        System.out.println("Available tracks:");
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] != null) {
+                System.out.println(tracks[i].getName() + " - Length: " + tracks[i].getLength() + " km. ");
+            }
+
+        }
+
+    }
 }
